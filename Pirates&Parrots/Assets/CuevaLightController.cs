e1e12e00ab1d27700b5ba66bb0d448a3e59dbcc8
franbarbero <fronkis99@gmail.com>
2020-06-13 17:34:42 +0200

    Montaje del templo y diversos asets

--- Pirates&Parrots/Assets/CuevaLightController.cs
+++ Pirates&Parrots/Assets/CuevaLightController.cs
@@ -9,18 +9,47 @@ public class CuevaLightController : MonoBehaviour
     public Animator SpotLight;
     public Animator SpotLight2;
 
+    float reduccion = 1;
+    bool Oscurece = false;
+    bool Amanece = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        bool Oscurece = false;
+    bool Amanece = false;
+        RenderSettings.skybox.SetFloat("_Exposure", 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Oscurece == true)
+        {
+            reduccion = reduccion - 0.015f;
+            RenderSettings.skybox.SetFloat("_Exposure", reduccion);
+            if (reduccion <= 0.12f)
+            {
+                reduccion = 0.11f;
+                RenderSettings.skybox.SetFloat("_Exposure", reduccion);
+                Oscurece = false;
+            }
+        }
+        
+        if (Amanece == true)
+        {
+            reduccion = reduccion + 0.015f;
+            RenderSettings.skybox.SetFloat("_Exposure", reduccion);
 
+            if (reduccion >= 0.99)
+            {
+                reduccion = 1;
+                RenderSettings.skybox.SetFloat("_Exposure", reduccion);
+                Amanece = false;
+            }
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,7 +61,7 @@ public class CuevaLightController : MonoBehaviour
             SpotLight2.SetBool("Apaga", true);
 
 
-
+            Oscurece = true;
         }
 
     }
@@ -45,10 +74,16 @@ public class CuevaLightController : MonoBehaviour
             SpotLight.SetBool("Apaga", false);
             SpotLight2.SetBool("Apaga", false);
 
-
+            Amanece = true;
 
         }
 
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        RenderSettings.skybox.SetFloat("_Exposure", 1f);
 
     }
 }

749d661ebd9acce0df1de793859df19b701efc80
franbarbero <fronkis99@gmail.com>
2020-06-15 05:33:21 +0200

    Cinematica Inicio

---
+++ Pirates&Parrots/Assets/CamerasController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerasController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject animatorCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorCamera.SetActive(true);
+        mainCamera.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    
+    public void cambioCamera()
+    {
+        animatorCamera.SetActive(false);
+        mainCamera.SetActive(true);
+    }
+}
